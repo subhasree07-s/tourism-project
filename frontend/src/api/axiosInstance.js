0e4c8572226b658b4b1e837b2dc6076b68b4965d
subhasree07-s <subhasreesahu007@gmail.com>
2026-04-19 09:58:32 +0530

    fix build error

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const BASE_URL = process.env.REACT_APP_API_URL;
+console.log("BASE_URL:", BASE_URL);
 
 const api = axios.create({
   baseURL: BASE_URL,
